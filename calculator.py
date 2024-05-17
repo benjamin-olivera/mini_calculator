275ac56fbad2c58613b3fad024a01f94ab1ddab5
benjamin-olivera <benjamin.oliveracolonio@gmail.com>
2024-05-17 15:58:46 -0500

    dictionary for symbols

--- calculator.py
+++ calculator.py
@@ -4,8 +4,15 @@ n2 = int(input("Enter the second number: "))
 def add(n1, n2):
     return n1 + n2 
 def substract(n1,n2):
-    return n1 -n2
+    return n1 - n2
 def multiply(n1,n2):
-    return n1*n2
+    return n1 * n2
 def divide(n1,n2):
-    return n1/n2
+    return n1 / n2
+
+operation = {
+    "+": add, 
+    "-": substract,
+    "*": multiply,
+    "/": divide
+}
