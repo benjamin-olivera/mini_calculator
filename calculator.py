cdfb813c1074b495583da44a797234e88e11887d
benjamin-olivera <benjamin.oliveracolonio@gmail.com>
2024-05-17 15:53:04 -0500

    basic functions has been written

---
+++ calculator.py
@@ -0,0 +1,11 @@
+n1 = int(input("Enter the first number: "))
+n2 = int(input("Enter the second number: "))
+
+def add(n1, n2):
+    return n1 + n2 
+def substract(n1,n2):
+    return n1 -n2
+def multiply(n1,n2):
+    return n1*n2
+def divide(n1,n2):
+    return n1/n2
